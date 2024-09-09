1b916ffc3cda71afc068dfa6753fac3d61e2bb70
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-09-09 23:22:35 +0300

    Create welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:want_to_do/ui/pages/onboarding/onboarding_page.dart';
-import 'package:want_to_do/ui/pages/onboarding/onboarding_provider.dart';
+import 'package:want_to_do/ui/pages/welcome/welcome_provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: OnboardingProvider(),
+      home: WelcomeProvider(),
     );
   }
 }
---
+++ lib/ui/pages/welcome/welcome_view_model.dart
@@ -0,0 +1,3 @@
+import 'package:want_to_do/base/base_view_model.dart';
+
+class WelcomeViewModel extends BaseViewModel {}

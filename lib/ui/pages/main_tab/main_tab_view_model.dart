035783943100248120a478c96d369feb63b1cead
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-09-17 22:55:05 +0300

    Add default check task parameters

--- lib/ui/pages/main_tab/main_tab_view_model.dart
+++ lib/ui/pages/main_tab/main_tab_view_model.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:want_to_do/base/base_view_model.dart';
 
 class MainTabViewModel extends BaseViewModel {
+  TextEditingController taskTitleController = TextEditingController();
+  TextEditingController taskDescriptionController = TextEditingController();
+
   int _currentIndex = 0;
 
   set currentIndex(int value) {
@@ -9,4 +13,29 @@ class MainTabViewModel extends BaseViewModel {
   }
 
   int get currentIndex => _currentIndex;
+
+  String _taskTitleText = '';
+  String _taskDescriptionText = '';
+
+  set taskTitleText(String text) {
+    _taskTitleText = text;
+    notifyListeners();
+  }
+
+  String get taskTitleText => _taskTitleText;
+
+  set taskDescriptionText(String text) {
+    _taskDescriptionText = text;
+    notifyListeners();
+  }
+
+  String get taskDescriptionText => _taskDescriptionText;
+
+  bool checkTaskParameters() {
+    _taskTitleText = taskTitleController.text;
+    _taskDescriptionText = taskDescriptionController.text;
+    if (_taskTitleText.isNotEmpty && _taskDescriptionText.isNotEmpty)
+      return true;
+    return false;
+  }
 }
